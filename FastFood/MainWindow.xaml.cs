966584de39b81505c1c404a98fef34b0859ed7de
Alex Andrés Ferruz <alex2andres@gmail.com>
2020-01-28 13:29:35 +0100

    Login screen view end

--- FastFood/MainWindow.xaml.cs
+++ FastFood/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FastFood
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
