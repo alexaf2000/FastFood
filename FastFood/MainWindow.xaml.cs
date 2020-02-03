1c26254b688c4c670167680402074be6e7b014b4
Alex Andrés Ferruz <alex2andres@gmail.com>
2020-02-03 13:40:49 +0100

    Changed TextBox to PasswordBox and set login on press enter

--- FastFood/MainWindow.xaml.cs
+++ FastFood/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,26 +12,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-
 using FastFood.user_interface;
+using MySql.Data.MySqlClient;
 
-namespace FastFood
-{
+namespace FastFood {
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-            ConfigurationHandler Config = new ConfigurationHandler();
-            Console.WriteLine("Dato: "+Config.getSetting("database","Connection"));
+    public partial class MainWindow : Window {
+        public MainWindow () {
+            InitializeComponent ();
+            ConfigurationHandler Config = new ConfigurationHandler ();
+            Console.WriteLine ("Dato: " + Config.getSetting ("database", "Connection"));
         }
 
-        private void Login_ButtonClick(object sender, RoutedEventArgs e)
-        {
+        private void Login_ButtonClick (object sender, RoutedEventArgs e) {
+            Login ();
+        }
+
+        private void Login_EnterKey (object sender, KeyEventArgs e) {
+            if (e.Key == Key.Enter) {
+                Login ();
+            }
+        }
+
+        private void Login () {
             String username = "";
             String password = "";
             String query;
@@ -40,52 +44,39 @@ namespace FastFood
             MySqlCommand cmd;
             MySqlConnection con = null;
             username = usernameInput.Text;
-            password = passwordInput.Text;
+            password = passwordInput.Password;
 
-            try
-            {
-                ConfigurationHandler Config = new ConfigurationHandler();
-                String host = Config.getSetting("host", "Connection");
-                String port = Config.getSetting("port", "Connection");
-                String database = Config.getSetting("database", "Connection");
-                String user = Config.getSetting("username", "Connection");
-                String pass = Config.getSetting("password", "Connection");
-                ruta = "Data Source="+ host + ";port=" + port + ";Database=" + database + ";Uid="+user+";Password="+pass;
-                con = new MySqlConnection(ruta);
-                con.Open();
+            try {
+                ConfigurationHandler Config = new ConfigurationHandler ();
+                String host = Config.getSetting ("host", "Connection");
+                String port = Config.getSetting ("port", "Connection");
+                String database = Config.getSetting ("database", "Connection");
+                String user = Config.getSetting ("username", "Connection");
+                String pass = Config.getSetting ("password", "Connection");
+                ruta = "Data Source=" + host + ";port=" + port + ";Database=" + database + ";Uid=" + user + ";Password=" + pass;
+                con = new MySqlConnection (ruta);
+                con.Open ();
                 query = "SELECT password from users where username= ?username;";
-                cmd = new MySqlCommand(query, con);
-                cmd.Parameters.AddWithValue("?username", username);
+                cmd = new MySqlCommand (query, con);
+                cmd.Parameters.AddWithValue ("?username", username);
                 System.Data.IDataReader dr;
-                dr = cmd.ExecuteReader();
-                if (dr.Read())
-                {
-                    String user_hash = dr.GetString(0);
-                    bool valid = BCrypt.Net.BCrypt.Verify(password, user_hash);
-                    Console.WriteLine("Valor: "+valid);
+                dr = cmd.ExecuteReader ();
+                if (dr.Read ()) {
+                    String user_hash = dr.GetString (0);
+                    bool valid = BCrypt.Net.BCrypt.Verify (password, user_hash);
                     if (valid) {
-                        dashboard form = new dashboard();
-                        form.ShowDialog();
-                    }
-                    else { 
-                    
+                        dashboard form = new dashboard ();
+                        form.ShowDialog ();
+                    } else {
+
                     }
 
-                    
-                }
-                else
-                {
-                }
-            }
-            catch (Exception exc)
-            {
-                Console.WriteLine(exc.Message.ToString());
-            }
-            finally
-            {
-                if (con != null)
-                {
-                    con.Close();
+                } else { }
+            } catch (Exception exc) {
+                Console.WriteLine (exc.Message.ToString ());
+            } finally {
+                if (con != null) {
+                    con.Close ();
                 }
             }
 
