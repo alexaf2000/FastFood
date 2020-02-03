9721b120813dbd38d49d3d1bbc9a5e8cac3b6006
Alex Andrés Ferruz <alex2andres@gmail.com>
2020-02-03 12:36:21 +0100

    Add login function

--- FastFood/MainWindow.xaml.cs
+++ FastFood/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
+using FastFood.user_interface;
 
 namespace FastFood
 {
@@ -27,8 +30,54 @@ namespace FastFood
             Console.WriteLine("Dato: "+Config.getSetting("database","Connection"));
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Login_ButtonClick(object sender, RoutedEventArgs e)
         {
+            String username = "";
+            String password = "";
+            String query;
+            String ruta;
+            MySqlCommand cmd;
+            MySqlConnection con = null;
+            username = usernameInput.Text;
+            password = passwordInput.Text;
+
+            try
+            {
+                ConfigurationHandler Config = new ConfigurationHandler();
+                String host = Config.getSetting("host", "Connection");
+                String port = Config.getSetting("port", "Connection");
+                String database = Config.getSetting("database", "Connection");
+                String user = Config.getSetting("username", "Connection");
+                String pass = Config.getSetting("password", "Connection");
+                ruta = "Data Source="+ host + ";port=" + port + ";Database=" + database + ";Uid="+user+";Password="+pass;
+                con = new MySqlConnection(ruta);
+                con.Open();
+                query = "SELECT username from users where username= ?username and password=?password;";
+                cmd = new MySqlCommand(query, con);
+                cmd.Parameters.AddWithValue("?username", username);
+                cmd.Parameters.AddWithValue("?password", password);
+                System.Data.IDataReader dr;
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    dashboard form = new dashboard();
+                    form.ShowDialog();
+                }
+                else
+                {
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message.ToString());
+            }
+            finally
+            {
+                if (con != null)
+                {
+                    con.Close();
+                }
+            }
 
         }
     }
