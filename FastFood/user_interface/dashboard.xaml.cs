1a28d34b1fa1be660302a54787ebe54da35a4559
Alex A.F <alex2andres@gmail.com>
2020-02-08 11:57:40 +0100

    Add sidebar elements, design, icons and log-out function started

--- FastFood/user_interface/dashboard.xaml.cs
+++ FastFood/user_interface/dashboard.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -19,6 +20,11 @@ namespace FastFood.user_interface
     /// </summary>
     public partial class dashboard : Window
     {
+
+        private bool Expanded = false;
+        private bool ForceClose = false;
+
+
         public dashboard()
         {
             InitializeComponent();
@@ -27,17 +33,55 @@ namespace FastFood.user_interface
 
         private void Dashboard_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-           
-            MessageBoxResult reply = MessageBox.Show("Se va a cerrar FastFood.\n¿Estás seguro de que deseas salir?", "Cerrando FastFood", MessageBoxButton.YesNo,MessageBoxImage.Warning);
+            if (!ForceClose) {
+            MessageBoxResult reply = MessageBox.Show("Se va a cerrar FastFood.\n¿Estás seguro de que deseas salir?", "Cerrando FastFood", MessageBoxButton.YesNo, MessageBoxImage.Warning);
             if (reply == MessageBoxResult.Yes)
             {
                 App.Current.MainWindow.Close();
             }
-            else{
+            else
+            {
                 e.Cancel = true;
+            }
             }
         }
 
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            GridLength OpenSize = new GridLength(180, GridUnitType.Pixel);
+            sidebar.Width = OpenSize;
+            int from, to;
+            if (Expanded) {
+                from = 180;
+                to = 60;
+                Expanded = false;
+            }
+            else {
+                from = 60;
+                to = 180;
+                Expanded = true;
+            }
+            Storyboard storyboard = new Storyboard();
 
+            Duration duration = new Duration(TimeSpan.FromMilliseconds(200));
+            CubicEase ease = new CubicEase { EasingMode = EasingMode.EaseOut };
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.EasingFunction = ease;
+            animation.Duration = duration;
+            storyboard.Children.Add(animation);
+            animation.From = from;
+            animation.To = to;
+            Storyboard.SetTarget(animation, sidebar);
+            Storyboard.SetTargetProperty(animation, new PropertyPath("(ColumnDefinition.MaxWidth)"));
+
+            storyboard.Begin();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            ForceClose = true;
+            App.Current.MainWindow.Show();
+            this.Close();
+        }
     }
 }
