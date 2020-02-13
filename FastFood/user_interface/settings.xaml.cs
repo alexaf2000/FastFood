7c849c45afeeaf663ee09d33c7c9501680b1d5e2
Alex AF <alex2andres@gmail.com>
2020-02-14 00:20:04 +0100

    Add users settings (without modify function)

--- FastFood/user_interface/settings.xaml.cs
+++ FastFood/user_interface/settings.xaml.cs
@@ -23,7 +23,10 @@ namespace FastFood.user_interface
     /// Lógica de interacción para settings.xaml
     /// </summary>
     public partial class settings : Window
+
     {
+        String usersDatagridSelected;
+
         public settings()
         {
             InitializeComponent();
@@ -59,6 +62,7 @@ namespace FastFood.user_interface
                     usersDatagrid.ItemsSource = table.DefaultView;
                     usersDatagrid.AutoGenerateColumns = true;
                     usersDatagrid.CanUserAddRows = false;
+                    usersDatagrid.UpdateLayout();
                 }
                 catch (Exception exc)
                 {
@@ -73,8 +77,83 @@ namespace FastFood.user_interface
                 }
             }
         }
-               
-}
+
+        private void usersDatagrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataRowView row = (DataRowView)usersDatagrid.SelectedItems[0];
+            if(row != null)
+            {
+                userDelete.IsEnabled = true;
+                userModify.IsEnabled = true;
+                usersDatagridSelected = row["nombre de usuario"].ToString();
+            }
+            else
+            {
+                userDelete.IsEnabled = false;
+                userModify.IsEnabled = false;
+            }
+        }
+
+        private void userCreate_Click(object sender, RoutedEventArgs e)
+        {
+            new settings_addUser().ShowDialog();
+            Dispatcher.BeginInvoke((Action)(() => SettingsTabControl.SelectedIndex = SettingsTabControl.SelectedIndex));
+        }
+
+        private void userModify_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void userDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Vas a eliminar el usuario "+usersDatagridSelected+"\n¿Estás seguro?","¿Estás seguro?",MessageBoxButton.YesNo,MessageBoxImage.Exclamation);
+            if(result.Equals(MessageBoxResult.Yes))
+            {
+                String query;
+                MySqlCommand cmd;
+                MySqlConnection con = null;
+
+                try
+                {
+                    ConfigurationHandler Config = new ConfigurationHandler();
+                    String host = Config.getSetting("host", "Connection");
+                    String port = Config.getSetting("port", "Connection");
+                    String database = Config.getSetting("database", "Connection");
+                    String user = Config.getSetting("username", "Connection");
+                    String pass = Config.getSetting("password", "Connection");
+                    String ruta = "Data Source=" + host + ";port=" + port + ";Database=" + database + ";Uid=" + user + ";Password=" + pass;
+                    con = new MySqlConnection(ruta);
+                    con.Open();
+                    query = "DELETE FROM users where username = ?username;";
+                    cmd = new MySqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("?username", usersDatagridSelected);
+                    System.Data.IDataReader dr;
+                    dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        MessageBox.Show("Error al eliminar el usuario.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario eliminado correctamente.");
+                    }
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc.Message.ToString());
+                }
+                finally
+                {
+                    if (con != null)
+                    {
+                        con.Close();
+                    }
+                    Dispatcher.BeginInvoke((Action)(() => SettingsTabControl.SelectedIndex = SettingsTabControl.SelectedIndex));
+                }
+            }
+        }
+    }
 
 
 
---
+++ FastFood/user_interface/settings_addUser.xaml.cs
@@ -0,0 +1,82 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FastFood.user_interface
+{
+    /// <summary>
+    /// Lógica de interacción para settings_addUser.xaml
+    /// </summary>
+    public partial class settings_addUser : Window
+    {
+        public settings_addUser()
+        {
+            InitializeComponent();
+        }
+
+        private void userCreate_Click(object sender, RoutedEventArgs e)
+        {
+            if (userName.Text != "" && userUsername.Text != "" && userMail.Text != "" && userPassword.Password != "") {
+                String query;
+                MySqlCommand cmd;
+                MySqlConnection con = null;
+                try
+                {
+                    ConfigurationHandler Config = new ConfigurationHandler();
+                    String host = Config.getSetting("host", "Connection");
+                    String port = Config.getSetting("port", "Connection");
+                    String database = Config.getSetting("database", "Connection");
+                    String user = Config.getSetting("username", "Connection");
+                    String pass = Config.getSetting("password", "Connection");
+                    String ruta = "Data Source=" + host + ";port=" + port + ";Database=" + database + ";Uid=" + user + ";Password=" + pass;
+                    con = new MySqlConnection(ruta);
+                    con.Open();
+                    query = "INSERT INTO users (name,username, password,email) values (?name,?username,?password,?email);";
+                    cmd = new MySqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("?name", userName.Text);
+                    cmd.Parameters.AddWithValue("?username", userUsername.Text);
+                    cmd.Parameters.AddWithValue("?password", BCrypt.Net.BCrypt.HashPassword(userPassword.Password));
+                    cmd.Parameters.AddWithValue("?email", userMail.Text);
+                    System.Data.IDataReader dr;
+                    dr = cmd.ExecuteReader();
+                    Window parentWindow = Window.GetWindow(this);
+                    if (dr.Read())
+                    {
+                        MessageBox.Show("Error al añadir usuario.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario añadido correctamente.");
+                        this.Close();
+                    }
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc.Message.ToString());
+                }
+                finally
+                {
+                    if (con != null)
+                    {
+                        con.Close();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Has de completar todos los campos","Datos incompletos",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+    }
+}
