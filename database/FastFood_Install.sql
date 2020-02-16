d1bbc3cb394ce902220571ae7b114556628ad20e
Alex A.F <alex2andres@gmail.com>
2020-02-16 22:53:23 +0100

    Update sql - Add all basic and necessaries tables for restaurant management

--- database/FastFood_Install.sql
+++ database/FastFood_Install.sql
@@ -3,9 +3,65 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Base de datos: `fastfood`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `consumitions`
+--
+
+CREATE TABLE `consumitions` (
+  `id` bigint(255) NOT NULL,
+  `serviceID` bigint(255) NOT NULL,
+  `productID` bigint(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(255) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `price` double(6,2) DEFAULT NULL,
+  `image` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `services`
+--
+
+CREATE TABLE `services` (
+  `id` bigint(255) NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tables`
+--
+
+CREATE TABLE `tables` (
+  `id` bigint(255) NOT NULL,
+  `description` varchar(150) DEFAULT NULL,
+  `actualServiceID` bigint(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -19,16 +75,45 @@ CREATE TABLE `users` (
   `username` varchar(20) NOT NULL,
   `password` varchar(150) NOT NULL,
   `email` varchar(60) NOT NULL,
-  `token` varchar(150)
+  `token` varchar(150) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `password`, `email`) VALUES
-(1, 'Administrador', 'admin', '$2a$11$WoBICGGe1NeSvEYiO5/K9eNsLLgdhcNe9ocWRAqWIxc5XUYikHubq', 'admin@fastfood.com');
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `email`, `token`) VALUES
+(1, 'Administrador', 'admin', '$2a$11$WoBICGGe1NeSvEYiO5/K9eNsLLgdhcNe9ocWRAqWIxc5XUYikHubq', 'admin@fastfood.com', '$2a$11$HjW4b2KWqfhSysOYb8YhuOZ.O3BR5DlBKmt770G3dANSCbxpQrkgK'),
+(13, 'alexito', 'alexito', '$2a$11$EZVdGwaGFTO.xwJcsqSBj.4VDzpCsY9cG.kvR2c1D4dz4Sb6JnTPC', 'alexito', '$2a$11$yEhIWUNWRDhgQH08ALU3UOQfjoScXriFMLQELYJzRz6/aYXTSyjY2');
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `consumitions`
+--
+ALTER TABLE `consumitions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indices de la tabla `tables`
+--
+ALTER TABLE `tables`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `users`
@@ -45,8 +130,24 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `consumitions`
+--
+ALTER TABLE `consumitions`
+  MODIFY `id` bigint(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` bigint(255) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
