69cf1550a0ad4a7dec5b1c97577fad208bfb4025
Alex AF <alex2andres@gmail.com>
2020-02-17 13:40:18 +0100

    Show tables dynamically from DB

--- FastFood/user_interface/pages/tables.xaml.cs
+++ FastFood/user_interface/pages/tables.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,8 +25,38 @@ namespace FastFood.user_interface.pages
         public tables()
         {
             InitializeComponent();
-            var numberButtons = Enumerable.Range(1, 30).Select(r => new KeyValuePair<string, bool>(r.ToString(), false)).ToList();
-            numberButtonItems.ItemsSource = numberButtons;
+            //var numberButtons = Enumerable.Range(1, 30).Select(r => new KeyValuePair<string, bool>(r.ToString(), false)).ToList();
+            //numberButtonItems.ItemsSource = numberButtons;
+
+
+            MySqlConnection con = null;
+            try
+            {
+                String query = "SELECT * FROM `tables`";
+                ConfigurationHandler Config = new ConfigurationHandler();
+                String host = Config.getSetting("host", "Connection");
+                String port = Config.getSetting("port", "Connection");
+                String database = Config.getSetting("database", "Connection");
+                String user = Config.getSetting("username", "Connection");
+                String pass = Config.getSetting("password", "Connection");
+                String ruta = "Data Source=" + host + ";port=" + port + ";Database=" + database + ";Uid=" + user + ";Password=" + pass;
+                con = new MySqlConnection(ruta);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "LoadDataBinding");
+                TablesList.DataContext = ds;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message.ToString());
+            }
+            finally
+            {
+                if (con != null)
+                {
+                    con.Close();
+                }
+            }
         }
 
         private void table_button_Loaded(object sender, RoutedEventArgs e)
